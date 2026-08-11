851298a73cff2bafe31f07e55ce7be4c77beb2b6
cardiojunkie <aswath9040@gmail.com>
2026-08-11 12:18:01 +0000

    Deployment Ready

--- fix_settings.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/hooks/useSettings.ts', 'utf-8');
-content = content.replace("maxPageContentLength: 100000", "maxPageContentLength: 40000");
-fs.writeFileSync('src/hooks/useSettings.ts', content);
